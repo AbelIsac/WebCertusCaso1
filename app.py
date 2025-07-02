60101ebfd1bac11c9cbd46e57ac94fdfc5a84f66
AbelIsac <abelichas@hotmail.com>
2025-07-01 22:06:09 -0500

    Primer avance de la web con Flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/acerca')
+def acerca():
+    return render_template('acerca.html')
+
+@app.route('/explorar')
+def explorar():
+    return render_template('explorar.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
